466d1c041f61959496b7dd71b7f8de4d15eb1fbc
KyahWel <williamcris.hod02@gmail.com>
2023-03-28 02:52:02 +0800

    Added chrome local storage API for retrieving previous extension state. Added autoreload function

--- button.js
+++ button.js
@@ -3,70 +3,110 @@ const sexyToggle = document.getElementById('sexy-toggle');
 const censorToggle = document.getElementById('censor-toggle');
 const getNumdetections = document.getElementById('numDetections');
 const eye = document.querySelector("#button i");
-let sexyIsChecked= false;
-let censorIsChecked= false;
+let isActive= false;
 
-button.addEventListener('click', (event) => {
-  let clickedButton;
-
-  //Condition to check if user clicked the eye or the actual button
-  if (event.target === eye) {
-    clickedButton = event.target.parentNode;
+// Retrieve button state from local storage
+chrome.storage.local.get('isActive', function (result) {
+  console.log(result.isActive);
+  isActive = result.isActive
+  if (result.isActive === true) {
+    
+    //set element updates here
   }
-  else {
-    clickedButton = event.target;
+  else{
+    // Clear 
+    chrome.storage.local.remove(["detections"]);
   }
- 
-  // Animation
-  clickedButton.classList.toggle('click-button-animation');
-  clickedButton.parentNode.classList.toggle('click-inner-animation');
-  clickedButton.parentNode.parentNode.classList.toggle('click-outer-animation');
+});
 
-  // Color
-  clickedButton.classList.toggle("button-enabled");
-  clickedButton.parentNode.classList.toggle('button-enabled-inner');
-  clickedButton.parentNode.parentNode.classList.toggle('button-enabled-outer');
+chrome.storage.local.get('isSexyToggled', function (result) {
+  console.log(result.isSexyToggled);
+  sexyToggle.checked = result.isSexyToggled;
+  if(sexyToggle.checked && censorToggle.checked){
+    censorToggle.checked = false;
+  }
+});
 
-  // Eye
-  eye.classList.toggle("bi-eye");
-  eye.classList.toggle("bi-eye-slash");
+chrome.storage.local.get('isCensorToggled', function (result) {
+  console.log(result.isCensorToggled);
+  censorToggle.checked = result.isCensorToggled;
+  if(censorToggle.checked && sexyToggle.checked){
+    sexyToggle.checked = false;
+  }
+});
 
-  //Body
-  document.body.classList.toggle('disabled');
+// Button functionalities
+document.addEventListener('DOMContentLoaded', function () {
 
-  setTimeout(() => {
-    clickedButton.classList.toggle('click-animation');
+  button.addEventListener('click', (event) => {
+    let clickedButton;
+    isActive = !isActive;
+    if(isActive){
+      chrome.tabs.query({}, function(tabs) {
+        tabs.forEach(function(tab) {
+          chrome.tabs.reload(tab.id, { bypassCache: true });
+        });
+      });
+    }
+    chrome.storage.local.set({ 'isActive': isActive });
+
+    //Condition to check if user clicked the eye or the actual button
+    if (event.target === eye) {
+      clickedButton = event.target.parentNode;
+    }
+    else {
+      clickedButton = event.target;
+    }
+    
+    // Animation
+    clickedButton.classList.toggle('click-button-animation');
     clickedButton.parentNode.classList.toggle('click-inner-animation');
     clickedButton.parentNode.parentNode.classList.toggle('click-outer-animation');
-  }, 700);
 
- 
-});
+    // Color
+    clickedButton.classList.toggle("button-enabled");
+    clickedButton.parentNode.classList.toggle('button-enabled-inner');
+    clickedButton.parentNode.parentNode.classList.toggle('button-enabled-outer');
 
-sexyToggle.addEventListener('change', function(e) {
-  if (this.checked) {
-    if(censorIsChecked) {
-      censorToggle.checked = false;
-      censorIsChecked = false;
+    // Eye
+    eye.classList.toggle("bi-eye-slash");
+    eye.classList.toggle("bi-eye");
+  
+    //Body
+    document.body.classList.toggle('disabled');
+
+    setTimeout(() => {
+      clickedButton.classList.toggle('click-animation');
+      clickedButton.parentNode.classList.toggle('click-inner-animation');
+      clickedButton.parentNode.parentNode.classList.toggle('click-outer-animation');
+    }, 700);
+
+  });
+
+  sexyToggle.addEventListener('change', function(e) {
+    if (this.checked) {
+      if(censorToggle.checked) {
+        censorToggle.checked = false;
+      }
+    } 
+    chrome.storage.local.set({ 'isSexyToggled': this.checked, 'isCensorToggled': censorToggle.checked});
+  });
+
+  censorToggle.addEventListener('change', function(e) {
+    if (this.checked) {
+      if(sexyToggle.checked) {
+        sexyToggle.checked = false;
+      }
+    } 
+    chrome.storage.local.set({ 'isSexyToggled': sexyToggle.checked, 'isCensorToggled': this.checked});
+  });
+
+  chrome.runtime.onMessage.addListener(function(request, sender, response) {
+    if(request.msg === "send") {
+      chrome.storage.local.get("numDetections", () => {
+        getNumdetections.innerText = numDetections.data;
+      });
     }
-    sexyIsChecked = true;
-  } 
+  }); 
+
 });
-
-censorToggle.addEventListener('change', function(e) {
-  if (this.checked) {
-    if(sexyIsChecked) {
-      sexyToggle.checked = false;
-      sexyIsChecked = false;
-    }
-    censorIsChecked = true;
-  } 
-});
-
-chrome.runtime.onMessage.addListener(function(request, sender, response) {
-  if(request.msg === "send") {
-    getNumdetections.innerText = request.data;
-  }
-}); 
-
-
--- contentScript.js
+++ contentScript.js
@@ -1,24 +1,71 @@
 var processedImages = [];
 var images = document.getElementsByTagName('img');
 let numDetections = 0;
+let sexyOption = false;
+let censorOption = false;
 
-function getNewImages() {
-  for (var i = 0; i < images.length; i++) {
-    if (processedImages.indexOf(images[i]) === -1) {
-        chrome.runtime.sendMessage({msg: 'image', index: i, url:images[i].src}, function({data,index}){
-          // Backlog: Add image size checker condition
-          console.log(data.Prediction[0].class);
-            if(data.Prediction[0].class === "nude" || data.Prediction[0].class === "sexy"){
-              numDetections ++;
-              chrome.runtime.sendMessage({msg:"send", data:numDetections})
-              console.log("is nude");
-              images[index].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
-            }
-        });
-        processedImages.push(images[i]);
-    }
-  }
-  setTimeout(getNewImages, 1000);
-}
+chrome.storage.local.get('isSexyToggled', function (result) {
+  sexyOption = result.isSexyToggled;
+});
 
-getNewImages();
+chrome.storage.local.get('isCensorToggled', function (result) {
+  censorOption = result.isSexyToggled;
+});
+
+chrome.storage.local.get('detections', function (result) {
+  numDetections = result.detections;
+});
+
+// function getNewImages() {
+//   for (var i = 0; i < images.length; i++) {
+//     if (processedImages.indexOf(images[i]) === -1) {
+//         if(!censorOption){
+//           chrome.runtime.sendMessage({msg: 'imageClassify', index: i, url:images[i].src}, function({data,index}){
+//             // Backlog: Add image size checker condition
+//             console.log(data.Prediction[0].class);
+//               if(sexyOption){
+//                 if(data.Prediction[0].class === "nude" || data.Prediction[0].class === "sexy"){
+//                   numDetections ++;
+//                   chrome.storage.local.set({'detections': numDetections});
+//                   images[index].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
+//                 }
+//                 else{
+//                   if(data.Prediction[0].class === "nude"){
+//                     numDetections ++;
+//                     chrome.storage.local.set({'detections': numDetections});
+//                     images[index].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
+//                   }
+//                 }
+//               }        
+//           });
+//         }
+//         else{
+//           //Check first if the image is nude then pass it to censor api
+//           chrome.runtime.sendMessage({msg: 'imageClassify', index: i, url:old_image_src}, function({data,index}){
+//             // Backlog: Add image size checker condition
+
+//             //Check if image is nude
+//             console.log(data.Prediction[0].class);
+//               if(data.Prediction[0].class === "nude"){
+//                 numDetections ++;
+//                 chrome.storage.local.set({'detections': numDetections});
+//                 // Save old image source
+//                 let old_image_src = images[index].src
+//                 images[index].src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/A_black_image.jpg/640px-A_black_image.jpg"
+//               }
+//               // Censor the image first then send message to censor API to 
+//               chrome.runtime.sendMessage({msg: 'imageCensor', index: i, url:old_image_src}, function({data,index}){
+//                 // Backlog: Add image size checker condition
+//                 // Add censor API result
+//                 //images[index].src = "s3 bukcet link"
+//               });
+//           });
+//         }
+       
+//         processedImages.push(images[i]);
+//     }
+//   }
+//   setTimeout(getNewImages, 1000);
+// }
+
+// getNewImages();
